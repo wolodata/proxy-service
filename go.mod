eaff8b0e50dfa610622846beee1084b31ef9c859
Jiaxin <songjiaxin2008@gmail.com>
2024-07-04 11:20:42 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,41 @@
+module proxy-service
+
+go 1.21
+
+toolchain go1.22.5
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/go-kratos/kratos/v2 v2.7.3
+	github.com/google/wire v0.6.0
+	github.com/sashabaranov/go-openai v1.26.2
+	go.uber.org/automaxprocs v1.5.3
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-kratos/aegis v0.2.0 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-playground/form/v4 v4.2.1 // indirect
+	github.com/google/subcommands v1.2.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/imdario/mergo v0.3.16 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.opentelemetry.io/otel v1.16.0 // indirect
+	go.opentelemetry.io/otel/metric v1.16.0 // indirect
+	go.opentelemetry.io/otel/trace v1.16.0 // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/tools v0.17.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240624140628-dc46fd24d27d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240617180043-68d350f18fd4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ internal/service/openai.go
@@ -0,0 +1,152 @@
+package service
+
+import (
+	"context"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/go-kratos/kratos/v2/errors"
+	"io"
+	"strings"
+
+	openai "github.com/sashabaranov/go-openai"
+
+	pb "proxy-service/api/proxy/v1"
+)
+
+type OpenAIService struct {
+	pb.UnimplementedOpenAIServer
+}
+
+func NewOpenAIService() *OpenAIService {
+	return &OpenAIService{}
+}
+
+func (s *OpenAIService) ChatCompletion(ctx context.Context, req *pb.ChatCompletionRequest) (*pb.ChatCompletionResponse, error) {
+	cfg := openai.DefaultConfig(req.GetToken())
+	cfg.BaseURL = req.GetUrl()
+
+	client := openai.NewClientWithConfig(cfg)
+
+	request := openai.ChatCompletionRequest{
+		Model:       req.GetModel(),
+		Messages:    make([]openai.ChatCompletionMessage, 0),
+		Temperature: req.GetTemperature(),
+		TopP:        req.GetTopP(),
+	}
+
+	for _, v := range req.GetMessages() {
+		var role string
+		switch v.GetRole() {
+		case pb.ChatCompletionMessageRole_CHAT_COMPLETION_MESSAGE_ROLE_UNSPECIFIED:
+			err := pb.ErrorInvalidRole("role: %s", v.GetRole().String())
+			return nil, err
+		case pb.ChatCompletionMessageRole_CHAT_COMPLETION_MESSAGE_ROLE_SYSTEM:
+			role = openai.ChatMessageRoleSystem
+		case pb.ChatCompletionMessageRole_CHAT_COMPLETION_MESSAGE_ROLE_USER:
+			role = openai.ChatMessageRoleUser
+		case pb.ChatCompletionMessageRole_CHAT_COMPLETION_MESSAGE_ROLE_ASSISTANT:
+			role = openai.ChatMessageRoleAssistant
+		}
+
+		content := strings.TrimSpace(v.GetContent())
+		if content == "" {
+			err := pb.ErrorEmptyContent("content: %s", v.GetContent)
+			return nil, err
+		}
+
+		request.Messages = append(request.Messages, openai.ChatCompletionMessage{
+			Role:    role,
+			Content: v.GetContent(),
+		})
+	}
+
+	response, err := client.CreateChatCompletion(ctx, request)
+	if err != nil {
+		err := pb.ErrorOpenaiError("CreateChatCompletion error: %s", err.Error())
+		return nil, err
+	}
+
+	if len(response.Choices) == 0 {
+		err := pb.ErrorNoChoice("", nil)
+		err = err.WithMetadata(map[string]string{
+			"response": spew.Sdump(response),
+		})
+		return nil, err
+	}
+
+	res := strings.TrimSpace(response.Choices[0].Message.Content)
+
+	return &pb.ChatCompletionResponse{
+		Content: res,
+	}, nil
+}
+func (s *OpenAIService) StreamChatCompletion(req *pb.StreamChatCompletionRequest, conn pb.OpenAI_StreamChatCompletionServer) error {
+	cfg := openai.DefaultConfig(req.GetToken())
+	cfg.BaseURL = req.GetUrl()
+
+	client := openai.NewClientWithConfig(cfg)
+
+	request := openai.ChatCompletionRequest{
+		Model:       req.GetModel(),
+		Messages:    make([]openai.ChatCompletionMessage, 0),
+		Temperature: req.GetTemperature(),
+		TopP:        req.GetTopP(),
+	}
+
+	for _, v := range req.GetMessages() {
+		var role string
+		switch v.GetRole() {
+		case pb.ChatCompletionMessageRole_CHAT_COMPLETION_MESSAGE_ROLE_UNSPECIFIED:
+			err := pb.ErrorInvalidRole("role: %s", v.GetRole().String())
+			return err
+		case pb.ChatCompletionMessageRole_CHAT_COMPLETION_MESSAGE_ROLE_SYSTEM:
+			role = openai.ChatMessageRoleSystem
+		case pb.ChatCompletionMessageRole_CHAT_COMPLETION_MESSAGE_ROLE_USER:
+			role = openai.ChatMessageRoleUser
+		case pb.ChatCompletionMessageRole_CHAT_COMPLETION_MESSAGE_ROLE_ASSISTANT:
+			role = openai.ChatMessageRoleAssistant
+		}
+
+		content := strings.TrimSpace(v.GetContent())
+		if content == "" {
+			err := pb.ErrorEmptyContent("content: %s", v.GetContent)
+			return err
+		}
+
+		request.Messages = append(request.Messages, openai.ChatCompletionMessage{
+			Role:    role,
+			Content: v.GetContent(),
+		})
+	}
+
+	chatCompletionStream, err := client.CreateChatCompletionStream(context.TODO(), request)
+	if err != nil {
+		err := pb.ErrorOpenaiError("CreateChatCompletionStream error: %s", err.Error())
+		return err
+	}
+
+	defer chatCompletionStream.Close()
+
+	for {
+		response, err := chatCompletionStream.Recv()
+		if errors.Is(err, io.EOF) {
+			return nil
+		}
+
+		if err != nil {
+			err := pb.ErrorOpenaiError("receive stream error: %s", err.Error())
+			return err
+		}
+
+		if len(response.Choices) == 0 {
+			err := pb.ErrorNoChoice("", nil)
+			err = err.WithMetadata(map[string]string{
+				"response": spew.Sdump(response),
+			})
+			return err
+		}
+
+		conn.Send(&pb.StreamChatCompletionResponse{
+			Chunk: response.Choices[0].Delta.Content,
+		})
+	}
+}
