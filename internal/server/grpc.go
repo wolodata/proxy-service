4f028cd15c9537f2dbb0ac5bd8733a8dac390605
Jiaxin <songjiaxin2008@gmail.com>
2024-07-04 13:23:33 +0800

    wip

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wolodata/proxy-service/internal/service"
 
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 )
@@ -15,6 +16,7 @@ func NewGRPCServer(c *conf.Server, openai *service.OpenAIService, logger log.Log
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
+			logging.Server(logger),
 		),
 	}
 	if c.Grpc.Network != "" {
